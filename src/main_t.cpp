e11d2e1ae3a6b109d5732bdf9c19cd1c4f903a74
yyhu_woozoom <hyy0289@woozoom.net>
2018-11-07 17:11:09 +0800

    RC.

--- src/main_t.cpp
+++ src/main_t.cpp
@@ -2,6 +2,9 @@
 #include <iostream>
 #include <string>
 
+#include <exception>
+#include <boost/exception/all.hpp>
+
 #include <opencv2/core.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/imgcodecs.hpp>
@@ -19,12 +22,178 @@ using namespace cv::xfeatures2d;
 using std::cout;
 using std::endl;
 
+struct exception_base : virtual std::exception, virtual boost::exception { };
+
+typedef boost::error_info<struct tag_info_string, std::string> ExceptionInfoString;
+
 const char* keys =
     "{ help h |                               | Print help message. }"
     "{ input1 | ../Data/1458400805.000000.jpg | Path to input image 1. }"
     "{ input2 | ../Data/1458400806.000000.jpg | Path to input image 2. }";
 
+typedef float real;
+
+class PairwiseHomography
+{
+public:
+    PairwiseHomography()
+    : mFlagSmooth(true), mSmoothKernelSize(7),
+      mMinHessian(5000), mMatchFilterRatio(0.6)
+    { }
+
+    ~PairwiseHomography() { }
+
+    void compute(InputArray _src, OutputArray _Hs)
+    {
+        if ( false == _src.isMatVector() )
+        {
+            // It is an error for now.
+            BOOST_THROW_EXCEPTION( exception_base() << ExceptionInfoString( "_src is expected to be an instance of std::vector<Mat>." ) );
+        }
+
+        if ( false == _Hs.isMatVector() )
+        {
+            // It is an error for now.
+            BOOST_THROW_EXCEPTION( exception_base() << ExceptionInfoString( "_Hs is expected to be an instance of std::vector<Mat>." ) );
+        }
+
+        std::vector<Mat> srcVec;
+        _src.getMatVector( srcVec );
+
+        std::vector<Mat> HsVec;
+        _Hs.getMatVector( HsVec );
+
+        Ptr<SURF> detector = SURF::create( mMinHessian );
+        std::vector<KeyPoint> keypoints1, keypoints2;
+        Mat descriptors1, descriptors2;
+
+        Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create(DescriptorMatcher::FLANNBASED);
+        std::vector< std::vector<DMatch> > knn_matches;
+
+        std::vector<DMatch> good_matches;
+        std::vector<Point2f> goodPnt1, goodPnt2;
+
+        Mat img1, img2, temp;
+        Mat H;
+
+        // Process img1.
+        img1 = srcVec.at(0).clone();
+
+        // Smooth.
+        if ( true == mFlagSmooth )
+        {
+            GaussianBlur( img1, temp, Size( mSmoothKernelSize, mSmoothKernelSize ), 0, 0, BORDER_CONSTANT); 
+            img1 = temp.clone();
+        }
+
+        // Detect feature points.
+        detector->detectAndCompute( img1, noArray(), keypoints1, descriptors1 );
+
+        std::vector<Mat>::iterator iter;
+        iter = srcVec.begin(); iter++;
+        int count = 1;
+
+        for ( ; iter != srcVec.end(); iter++ )
+        {
+            cout << "Process " << count << " pair." << endl;
+
+            img2 = (*iter).clone();
+
+            // Smooth.
+            if ( true == mFlagSmooth )
+            {
+                GaussianBlur( img2, temp, Size( mSmoothKernelSize, mSmoothKernelSize ), 0, 0, BORDER_CONSTANT); 
+                img2 = temp.clone();
+            }
+
+            // Detect feature points.
+            detector->detectAndCompute( img2, noArray(), keypoints2, descriptors2 );
+
+            // Match.
+            matcher->knnMatch( descriptors1, descriptors2, knn_matches, 2 );
+
+            std::cout << "Matches: " << knn_matches.size() << std::endl;
+
+            for (size_t i = 0; i < knn_matches.size(); i++)
+            {
+                if (knn_matches[i][0].distance < mMatchFilterRatio * knn_matches[i][1].distance)
+                {
+                    good_matches.push_back(knn_matches[i][0]);
+                    goodPnt1.push_back( keypoints1.at( knn_matches[i][0].queryIdx ).pt );
+                    goodPnt2.push_back( keypoints2.at( knn_matches[i][0].trainIdx ).pt );
+                }
+            }
+
+            std::cout << "Good matches: " << good_matches.size() << std::endl;
+
+            H = findHomography( goodPnt2, goodPnt1, RANSAC );
+
+            HsVec.push_back( H.clone() );
+
+            std::cout << "H = " << std::endl;
+            std::cout << H << std::endl;
+
+            // Interchange values.
+            img1 = img2.clone();
+            keypoints1 = keypoints2;
+            descriptors1 = descriptors2;
+
+            keypoints2.clear();
+            descriptors2.release();
+
+            knn_matches.clear();
+            good_matches.clear();
+            goodPnt1.clear();
+            goodPnt2.clear();
+        }
+    }
+
+public:
+    bool mFlagSmooth;
+    int mSmoothKernelSize;
+    int mMinHessian;
+    real mMatchFilterRatio;
+};
+
+int main_backup( int argc, char* argv[] );
+
 int main( int argc, char* argv[] )
+{
+    CommandLineParser parser( argc, argv, keys );
+
+    std::string imgFn1 = parser.get<String>("input1");
+    std::string imgFn2 = parser.get<String>("input2");
+
+    Mat img1 = imread( imgFn1, IMREAD_GRAYSCALE );
+    Mat img2 = imread( imgFn2, IMREAD_GRAYSCALE );
+
+    if ( img1.empty() || img2.empty() )
+    {
+        cout << "Could not open or find the image!\n" << endl;
+        parser.printMessage();
+        return -1;
+    }
+
+    std::vector<Mat> inputMats;
+    std::vector<Mat> Hs;
+
+    inputMats.push_back( img1 );
+    inputMats.push_back( img2 );
+
+    PairwiseHomography ph;
+    ph.compute( inputMats, Hs );
+
+    return 0;
+}
+#else
+int main()
+{
+    std::cout << "This tutorial code needs the xfeatures2d contrib module to be run." << std::endl;
+    return 0;
+}
+#endif
+
+int main_backup( int argc, char* argv[] )
 {
     CommandLineParser parser( argc, argv, keys );
 
@@ -168,10 +337,3 @@ int main( int argc, char* argv[] )
     waitKey();
     return 0;
 }
-#else
-int main()
-{
-    std::cout << "This tutorial code needs the xfeatures2d contrib module to be run." << std::endl;
-    return 0;
-}
-#endif
